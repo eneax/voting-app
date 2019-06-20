2a53ea595f3f8270eb82b06d2c775fc231ae814a
eneax <19818937+eneax@users.noreply.github.com>
2019-06-20 09:51:23 +0200

    Refactor Product component

--- public/js/app.js
+++ public/js/app.js
@@ -59,15 +59,9 @@ class ProductList extends React.Component {
 
 
 class Product extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
-
-  handleUpVote() {
-    this.props.onVote(this.props.id);
-  }
+  handleUpVote = () => (
+    this.props.onVote(this.props.id)
+  );
 
   render() {
     return (
